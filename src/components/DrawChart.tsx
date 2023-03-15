a8de81a7a99c024b66136e01ef82d911079a0657
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-03-15 19:48:25 +0900

    fix: chartjs import position

--- src/components/DrawChart.tsx
+++ src/components/DrawChart.tsx
@@ -1,8 +1,30 @@
 import { Chart } from 'react-chartjs-2';
 import { useEffect, useState } from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  PointElement,
+  LineElement,
+  BarElement,
+  Legend,
+  Colors,
+} from 'chart.js';
 import { ChartDataType } from '../types/ChartDataType';
 import convertChartData from '../libs/convertChartData';
 import getChartData from '../services/getChartData';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  PointElement,
+  LineElement,
+  BarElement,
+  Legend,
+  Colors
+);
 
 const DrawChart = () => {
   const [data, setData] = useState<ChartDataType | null>(null);
--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  Tooltip,
-  PointElement,
-  LineElement,
-  BarElement,
-  Legend,
-  Colors,
-} from 'chart.js';
 import App from './App';
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  Tooltip,
-  PointElement,
-  LineElement,
-  BarElement,
-  Legend,
-  Colors
-);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />
